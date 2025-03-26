17442c395d1a1b58a11eba0347fa5e8c6a82eb9b
momo-mori <moomoo_pom2828@yahoo.co.jp>
2025-03-27 06:39:21 +0900

    STOP_SPEED定数の定義、if文に前述定義時の処置の追加

--- sample3.rb
+++ sample3.rb
@@ -1,6 +1,9 @@
 class Car 
   #車の最大速度を定義
   MAX_SPEED = 180
+  #停車時のスピードの表示
+  STOP_SPEED = 0
+
   #車の色、モデルの変数を持つ
   def initialize(color, model)
     @color = color
@@ -13,9 +16,13 @@ class Car
     puts "車の色は#{@color}で、モデルは#{@model}です"
   end
 
-  #走行可能なスピードの設定
+  #走行スピードの表示
   def set_speed(speed)
-    if speed <= MAX_SPEED
+    #速度0km/h時の表示
+    if speed == STOP_SPEED
+      puts "停車しています。" 
+    elsif
+      speed <= MAX_SPEED
       @speed = speed
     else
       puts "スピードは#{MAX_SPEED}km/hで設定してください。"
@@ -31,5 +38,5 @@ end
 #レクサスインスタンスの定義
 lexus_car = Car.new("gray", "lexus")
 lexus_car.show_car
-lexus_car.set_speed(180)
+lexus_car.set_speed(190)
 lexus_car.show_car_speed
